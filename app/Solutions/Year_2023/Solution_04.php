b662f69ce09918a3ed9ab79ac8e9962f34557f61
Aurelien-DRUON <aurelien.druon@epfedu.fr>
2023-12-04 14:56:41 +0100

    ff la team

--- app/Solutions/Year_2023/Solution_04.php
+++ app/Solutions/Year_2023/Solution_04.php
@@ -6,7 +6,28 @@ class Solution_04
 {
     public function silver(string $data): string
     {
-        return 'todo';
+        $result = 0;
+        $data = explode("\n", $data);
+        array_pop($data);
+        foreach ($data as $line) {
+            $lineresult = 0;
+            $nums = explode("|", $line);
+            $cardnum = explode(":", $nums[0]);
+            $winnums = explode(" ", $cardnum[1]);
+            $winnums = array_filter($winnums, function ($value) {
+                return $value !== "";
+            });
+            $getnums = explode(" ", $nums[1]);
+            foreach ($getnums as $getnum) {
+                if (in_array($getnum, $winnums)) {
+                    $lineresult += 1;
+                }
+            }
+            if ($lineresult > 0) {
+                $result += 2 ** ($lineresult - 1);
+            }
+        }
+        return $result;
     }
 
     public function gold(string $data): string
